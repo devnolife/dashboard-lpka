5a0363d15b15eb1ebfc31d58c8dd7a036df796f9
unknown <105841111221@student.unismuh.ac.id>
2024-11-18 21:45:51 +0800

    Done 3 things

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,55 +1,87 @@
+"use client";
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
+import 'flowbite';
 
 export default function HeroSection() {
+  const [navbarHeight, setNavbarHeight] = useState(0);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      import('flowbite').then((flowbite) => {
+      });
+
+      const navbar = document.querySelector('nav');
+      if (navbar) {
+        setNavbarHeight(navbar.clientHeight);
+      }
+    }
+  }, []);
+
   return (
-    <div id="indicators-carousel" className="relative w-full" data-carousel="static">
+    <div
+      id="indicators-carousel"
+      className="relative w-full"
+      data-carousel="static"
+      style={{ paddingTop: navbarHeight }}
+    >
       {/* Carousel wrapper */}
-      <div className="relative h-56 overflow-hidden rounded-lg md:h-96">
+      <div className="relative h-56 md:h-96 overflow-hidden rounded-lg">
         {/* Item 1 */}
-        <div className="block duration-700 ease-in-out" data-carousel-item="active">
-          <div className="relative h-full w-full">
-            <Image
-              src="/image/hero1.jpg"
-              alt="Deskripsi gambar"
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
+        <div
+          className="absolute inset-0 transition-transform duration-700 ease-in-out transform translate-x-0"
+          data-carousel-item="active"
+        >
+          <Image
+            src="/image/gedung_crop.png"
+            alt="Deskripsi gambar"
+            fill
+            sizes="(min-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            style={{ objectFit: 'cover' }}
+            priority
+          />
         </div>
         {/* Item 2 */}
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-          <div className="relative h-full w-full">
-            <Image
-              src="/image/hero1.jpg"
-              alt="Deskripsi gambar"
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
+        <div
+          className="absolute inset-0 hidden transition-transform duration-700 ease-in-out transform translate-x-full"
+          data-carousel-item
+        >
+          <Image
+            src="/image/hero2.jpg"
+            alt="Deskripsi gambar"
+            fill
+            sizes="(min-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            style={{ objectFit: 'cover' }}
+          />
         </div>
         {/* Item 3 */}
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-          <div className="relative h-full w-full">
-            <Image
-              src="/image/hero1.jpg"
-              alt="Deskripsi gambar"
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
+        <div
+          className="absolute inset-0 hidden transition-transform duration-700 ease-in-out transform -translate-x-full"
+          data-carousel-item
+        >
+          <Image
+            src="/image/hero1.jpg"
+            alt="Deskripsi gambar"
+            fill
+            sizes="(min-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            style={{ objectFit: 'cover' }}
+          />
         </div>
         {/* Item 4 */}
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-          <div className="relative h-full w-full">
-            <Image
-              src="/image/hero1.jpg"
-              alt="Deskripsi gambar"
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
+        <div
+          className="absolute inset-0 hidden transition-transform duration-700 ease-in-out transform translate-x-full"
+          data-carousel-item
+        >
+          <Image
+            src="/image/hero2.jpg"
+            alt="Deskripsi gambar"
+            fill
+            sizes="(min-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            style={{ objectFit: 'cover' }}
+          />
         </div>
       </div>
+      
       {/* Carousel indicators */}
       <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
         <button type="button" className="w-3 h-3 rounded-full bg-white" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
@@ -57,6 +89,7 @@ export default function HeroSection() {
         <button type="button" className="w-3 h-3 rounded-full bg-white/50 hover:bg-white" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
         <button type="button" className="w-3 h-3 rounded-full bg-white/50 hover:bg-white" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
       </div>
+      
       {/* Slider controls */}
       <button
         type="button"
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="fixed top-0 w-full bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+    <nav className="fixed top-0 w-full z-50 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="image/logo-unismuh.png" className="h-8" alt="Flowbite Logo" />
